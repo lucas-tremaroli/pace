4f76c3ef99c7867527adb093729685f767eb5795
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2026-01-24 00:10:47 +0100

    feat(cmd): add version flag
    Signed-off-by: lucas-tremaroli <lucasluigitremaroli@outlook.com>

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "github.com/lucas-tremaroli/pace/cmd"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
